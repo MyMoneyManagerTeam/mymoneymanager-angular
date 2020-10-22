11f75736deec83a5b079402288379d9aa9a8057d
Alex <alexian.moins@outlook.com>
2020-10-22 17:31:38 +0200

    Inscription

--- MyMoneyManager/src/app/home-page/hp-register/hp-register.component.ts
+++ MyMoneyManager/src/app/home-page/hp-register/hp-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-hp-register',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hp-register.component.css']
 })
 export class HpRegisterComponent implements OnInit {
+   form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    FormBuilder: this.form = this.fb.group({
+      email: ['', [
+        Validators.required,
+        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]
+      ],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(6)]
+      ],
+      confirmPassword: ['', Validators.required],
+      tel: ['', Validators.required],
+      pays: ['', Validators.required],
+      adresse: ['', Validators.required],
+      codePostal: ['', Validators.required],
+      ville: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  register() {
+
+  }
 }
