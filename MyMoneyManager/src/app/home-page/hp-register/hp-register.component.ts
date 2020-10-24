2676180d22bde7a51021a4b4fd52a405687732fb
Lucas <lopez.lucas@hotmail.com>
2020-10-24 03:28:33 +0200

    Ajout d'un select pour les pays + ajout lecture json module

--- MyMoneyManager/src/app/home-page/hp-register/hp-register.component.ts
+++ MyMoneyManager/src/app/home-page/hp-register/hp-register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import countries from '/src/assets/JSON/country.json';
 
 @Component({
   selector: 'app-hp-register',
@@ -8,12 +9,13 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class HpRegisterComponent implements OnInit {
    form: FormGroup;
+   countryList: { name: string, Dial_Code: string, ISO_Code: string }[] = countries;
 
   constructor(private fb: FormBuilder) {
     FormBuilder: this.form = this.fb.group({
       email: ['', [
         Validators.required,
-        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
       ],
       password: ['', [
         Validators.required,
@@ -26,6 +28,7 @@ export class HpRegisterComponent implements OnInit {
       codePostal: ['', Validators.required],
       ville: ['', Validators.required]
     });
+
   }
 
   ngOnInit(): void {
